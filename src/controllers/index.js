2df40dc36794113c49cae84a5c163e82ca2cc968
Neskz <44223062+Neskz@users.noreply.github.com>
2018-10-24 09:52:00 +0200

    SRC

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,8 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { pizzas: [{ name: "test" }, { name: "test2" }] });
+  res.render("index", { pizzas: [{ img: "/static/pepperoni.jpg", name: "Pepperoni", prix: "8,50" }, { img: "/static/hypnotika.png", name: "Hypnotika", prix: "9,50" }, { img: "/static/savoyarde.png", name: "Savoyarde", prix: "9" },
+  { img: "/static/fromag.png", name: "4 fromages", prix: "9,25" }, { img: "/static/canib.png", name: "Cannibale", prix: "9,75" }, { img: "/static/bacon.png", name: "Bacon Groovy", prix: "8,99" }] });
 });
 
 router.post("/", (req, res) => {
